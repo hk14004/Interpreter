79306408dd12a5a6884a142013fd28b4e79cbb7c
Hardijs Kirsis <hardijs.kirsis@seb.lv>
2024-06-30 16:59:08 +0300

    Add loops

--- Java/JLOX/src/com/craftinginterpreters/lox/Expr.java
+++ Java/JLOX/src/com/craftinginterpreters/lox/Expr.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 
+import java.util.List;
+
 abstract class Expr {
     interface Visitor<R> {
         R visitAssignExpr(Assign expr);
@@ -9,6 +11,8 @@ abstract class Expr {
         R visitGroupingExpr(Grouping expr);
 
         R visitLiteralExpr(Literal expr);
+
+        R visitLogicalExpr(Logical expr);
 
         R visitUnaryExpr(Unary expr);
 
@@ -71,6 +75,23 @@ abstract class Expr {
         }
 
         final Object value;
+    }
+
+    static class Logical extends Expr {
+        Logical(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLogicalExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
     }
 
     static class Unary extends Expr {
