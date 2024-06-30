4210ebfcbd9db484968c3be4346f01838f7a9bf7
Hardijs Kirsis <hardijs.kirsis@seb.lv>
2024-06-30 17:10:52 +0300

    Use hardcoded program input

--- Java/JLOX/src/com/craftinginterpreters/lox/Lox.java
+++ Java/JLOX/src/com/craftinginterpreters/lox/Lox.java
@@ -14,14 +14,7 @@ public class Lox {
     static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
-        }
+        runFile("src/com/craftinginterpreters/lox/program.lox");
     }
 
     private static void runFile(String path) throws IOException {
